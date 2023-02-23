707fd2031f1f0e1aced1fa69cc1563e7f60ef538
oleks <oleks@DESKTOP-IME9DUS>
2023-02-23 21:40:44 +0200

    Add project files.

---
+++ Fractions/FractWithIntPart.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fractions
+{
+    public class FractionWithIntPart : Fraction
+    {
+        public int IntPart { get; private set; }
+
+
+        public FractionWithIntPart(int intPart, int numerator, int denominator) : base(numerator, denominator)
+        {
+            IntPart = intPart;
+            ConvertFraction();
+        }
+        public FractionWithIntPart(int numerator, int denominator) : base(numerator, denominator)
+        {
+            IntPart = 0;
+            ConvertFraction();
+        }
+        public FractionWithIntPart(FractionWithIntPart fraction) : base(fraction)
+        {
+            IntPart = fraction.IntPart;
+            ConvertFraction();
+        }
+        public FractionWithIntPart(Fraction fraction) : base(fraction)
+        {
+            ConvertFraction();
+        }
+        protected void ConvertFraction()
+        {
+            if (Numerator >= Denominator)
+            {
+                IntPart += (int)(Numerator / Denominator);
+                Numerator %= Denominator;
+            }
+        }
+        public static void ConvertFraction(FractionWithIntPart a)
+        {
+            if (a.Numerator >= a.Denominator)
+            {
+                a.IntPart += (a.Numerator / a.Denominator);
+                a.Numerator %= a.Denominator;
+            }
+        }
+        public FractionWithIntPart(int intPart) : this(intPart, 0, 1)
+        {
+        }
+        public override string ToString() 
+        {
+            if (IntPart == 0)
+            {
+               return base.ToString();
+            }
+            else
+            {
+                return $"{IntPart} {base.ToString()}";
+            }
+        }
+        public static FractionWithIntPart operator +(FractionWithIntPart a) => a;
+        public static FractionWithIntPart operator -(FractionWithIntPart a) => new FractionWithIntPart(-a.IntPart, -a.Numerator, a.Denominator);
+        public static FractionWithIntPart operator +(FractionWithIntPart a, FractionWithIntPart b) => new FractionWithIntPart(a.IntPart + b.IntPart, a.Numerator * b.Denominator + b.Numerator * a.Denominator, a.Denominator * b.Denominator);
+        public static FractionWithIntPart operator +(FractionWithIntPart a, int b) => new FractionWithIntPart(a.IntPart + b, a.Numerator, a.Denominator);
+        public static FractionWithIntPart operator -(FractionWithIntPart a, FractionWithIntPart b) => a + (-b);
+        public static FractionWithIntPart operator -(FractionWithIntPart a, int b)
+        {
+            var temp = new FractionWithIntPart(b - a.Denominator / a.Denominator, a.Denominator, a.Denominator);
+            return a - temp;
+
+        }
+        public static FractionWithIntPart operator *(FractionWithIntPart a, FractionWithIntPart b)
+        {
+            var den = a.Denominator * b.Denominator;
+
+            var num = (a.Numerator + (a.IntPart * a.Denominator)) * (b.Numerator + (b.IntPart * b.Denominator));
+
+            var product = new FractionWithIntPart(0, num, den);
+
+            ConvertFraction(product);
+
+            return product;
+
+        }
+        public static FractionWithIntPart operator /(FractionWithIntPart a, FractionWithIntPart b)
+        {
+            var num = (a.Numerator + (a.IntPart * a.Denominator)) * b.Denominator;
+
+            var den = (b.Numerator + (b.IntPart * b.Denominator)) * a.Denominator;
+
+            var quotient = new FractionWithIntPart(0, num, den);
+
+            ConvertFraction(quotient);  
+
+            return quotient;
+        }
+    }
+}
+    
---
+++ Fractions/Fractions.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fractions
+{
+    public class Fraction
+    {
+        public int Numerator { get; protected set; }
+        public int Denominator { get; protected set; }
+
+        public Fraction(int numerator, int denominator)
+        {
+            if (denominator == 0)
+            {
+                throw new ArgumentException("you can't divide by zero", nameof(denominator));
+            }
+            if (denominator < 0)
+            {
+                throw new ArgumentException("denominator can't be less than zero", nameof(denominator));
+            }
+            Numerator = numerator;
+            Denominator = denominator;
+
+        }
+        public Fraction(Fraction fraction) : this(fraction.Numerator, fraction.Denominator)
+        {
+
+        }
+        protected static void ReduceTheFraction(Fraction a)
+        {
+            int FindGDC(Fraction a)
+            {
+                int num = a.Numerator;
+                int den = a.Denominator;
+                while (den != 0)
+                {
+                    int remainder = num % den;
+                    num = den;
+                    den = remainder;
+                }
+                return num;
+            }
+            int GCD = FindGDC(a);
+            while (GCD != 1)
+            {
+                a.Numerator /= GCD;
+                a.Denominator /= GCD;
+                GCD = FindGDC(a);
+            }
+        }
+        public override string ToString() => $"{Numerator}/{Denominator}";
+
+        public static Fraction operator +(Fraction a) => a;
+        public static Fraction operator -(Fraction a) => new Fraction(-a.Numerator, a.Denominator);
+        public static Fraction operator +(Fraction a, Fraction b)
+        {
+            Fraction sum = new Fraction(a.Numerator * b.Denominator + b.Numerator * a.Denominator, a.Denominator * b.Denominator);
+            ReduceTheFraction(sum);
+            return sum;
+        }
+
+
+        public static Fraction operator +(Fraction a, int b)
+        {
+            var temp = new Fraction(a.Denominator * b, a.Denominator);
+            var sum = a + temp;
+            ReduceTheFraction(sum);
+            return sum;
+        }
+        public static Fraction operator -(Fraction a, Fraction b) => a + (-b);
+        public static Fraction operator -(Fraction a, int b)
+        {
+            var temp = new Fraction(a.Denominator * b, a.Denominator);
+            return a - temp;
+        }
+
+        public static Fraction operator *(Fraction a, Fraction b) => new Fraction(a.Numerator * b.Numerator, a.Denominator * b.Denominator);
+        public static Fraction operator *(Fraction a, int b)
+        {
+            var temp = new Fraction(a.Denominator * b, a.Denominator);
+            return a * temp;
+        }
+        public static Fraction operator /(Fraction a, Fraction b)
+        {
+            var num = a.Numerator * b.Denominator;
+            var den = a.Denominator * b.Numerator;
+            if (den < 0)
+            {
+                num *= -1;
+                den *= -1;
+            }
+            return new Fraction(num, den);
+        }
+
+        public static Fraction operator /(Fraction a, int b)
+        {
+            var temp = new Fraction(a.Denominator * b, a.Denominator);
+            return a / temp;
+        }
+    }
+}
+
+
