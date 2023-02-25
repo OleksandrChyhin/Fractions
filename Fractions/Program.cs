cc7cc6fff33471d8170611d7812745d0dab40c16
OleksandrChyhin <106186303+OleksandrChyhin@users.noreply.github.com>
2023-02-25 18:52:49 +0200

    Update Program.cs

--- Fractions/Program.cs
+++ Fractions/Program.cs
@@ -5,10 +5,3 @@ using System.Text;
 using System.Threading.Tasks;
 using Fractions;
 
-Fraction a = new Fraction( 19, 3);
-
-Fraction b = new Fraction(2,3);
-
-Fraction c = new Fraction(b);
-
-Console.WriteLine((c).ToString());
